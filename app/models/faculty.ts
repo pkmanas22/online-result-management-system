0f93f4c3238116f1e72d268978099004006958c1
subas <subasmohanty256@gmail.com>
2025-01-06 00:24:21 +0530

    feat : add mongo models

---
+++ app/models/faculty.ts
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const facultySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+  },
+  department: {
+    type: String,
+    required: true,
+  },
+  contactNumber: {
+    type: Number,
+  },
+  passwordUpdated: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+export default mongoose.model("faculties", facultySchema);
